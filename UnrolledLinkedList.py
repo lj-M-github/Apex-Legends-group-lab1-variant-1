d35ceb18eb0f4d56f0af21ec8f08f66e526b8113
RaganaBloodedge <1806015345@qq.com>
2025-02-18 15:07:01 +0300

    Some basic functions

--- UnrolledLinkedList.py
+++ UnrolledLinkedList.py
@@ -1,13 +1,29 @@
-#class Node:
+class Node():
+    def __init__(self, elements = None):
+        self.elements = elements if elements is not None else []
+        self.next  = None
+
 class UnrolledLinkedList():
-    def hello(self):
-        return "hello"
+    def __init__(self, size):
+        self.head = None
+        self.size = size
+        self.current_node = None
+        self.current_index = 0
 
-    def add(self, a, b):
-        return a + b
+    def append(self, value):
+        if self.head is None:
+            self.head = Node([value])
+            self.current_node = self.head
+            self.current_index = 1
+        else:
+            if len(self.current_node.elements) < self.size:
+                self.current_node.elements.append(value)
+                self.current_index += 1
+            else:
+                new_node = Node([value])
+                self.current_node.next = new_node
+                self.current_node = new_node
+                self.current_index = 1
 
-    def sub(self, a, b):
-        return a - b
-    
-    def add(self, a, b):
-        return a + b
+
+
