c7546e02eb3dc7c6e9fc5dfbf0d27c048c50bf89
RaganaBloodedge <1806015345@qq.com>
2025-02-19 18:56:27 +0300

    Add get and del

--- UnrolledLinkedList.py
+++ UnrolledLinkedList.py
@@ -2,6 +2,7 @@ class Node():
     def __init__(self, elements = None):
         self.elements = elements if elements is not None else []
         self.next  = None
+        self.last  = None
 
 class UnrolledLinkedList():
     def __init__(self, size):
@@ -22,8 +23,29 @@ class UnrolledLinkedList():
             else:
                 new_node = Node([value])
                 self.current_node.next = new_node
+                new_node.last = self.current_node
                 self.current_node = new_node
                 self.current_index = 1
+    
+    def getIndex(self):
+        if self.current_node is None:
+            return None, 0
+        else:
+            return self.current_node.elements, self.current_index
+    
+    def delElement(self):
+        if self.current_node is None or self.current_index == 0:
+            return False
+        
+        del self.current_node.elements[self.current_index - 1]
+
+        if len(self.current_node.elements) == 0:
+            if self.current_node.last:
+                self.current_node = self.current_node.last
+            self.current_index = len(self.current_node.elements)
+        else:
+            self.current_index -= 1
+        return True
 
 
 
