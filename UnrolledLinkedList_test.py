e7e1462eddad51970fc9bd9c40d38c956b074f4b
RaganaBloodedge <1806015345@qq.com>
2025-02-19 19:11:07 +0300

    Part test functions

--- UnrolledLinkedList_test.py
+++ UnrolledLinkedList_test.py
@@ -1,14 +1,45 @@
 import unittest
 from hypothesis import given,  strategies
-
 from UnrolledLinkedList import UnrolledLinkedList
 
 
 class TestFoo(unittest.TestCase):
 
-    def test_hello(self):
-        self.assertEqual(UnrolledLinkedList().hello(), "hello")
+    def test_append(self):
+        ull = UnrolledLinkedList(size=2)
+        ull.append(1)
+        ull.append(2)
+        self.assertEqual(ull.getIndex(), ([1, 2], 2))
+        ull.append(3)
+        self.assertEqual(ull.getIndex(), ([3], 1))
+
+    def test_getIndex(self):
+        ull = UnrolledLinkedList(size=2)
+        ull.append(1)
+        ull.append(2)
+        self.assertEqual(ull.getIndex(), ([1, 2], 2))
+        ull.append(3)
+        self.assertEqual(ull.getIndex(), ([3], 1))
+
+    def test_delElement(self):
+        ull = UnrolledLinkedList(size=2)
+        ull.append(1)
+        ull.append(2)
+        ull.append(3)
+        self.assertTrue(ull.delElement())
+        self.assertEqual(ull.getIndex(), ([2], 1))
+        self.assertTrue(ull.delElement())
+        self.assertEqual(ull.getIndex(), ([], 0))
+        self.assertFalse(ull.delElement())     
 
     @given(strategies.integers(), strategies.integers())
     def test_add_commutative(self, a, b):
-        self.assertEqual(UnrolledLinkedList().add(a, b), UnrolledLinkedList().add(b, a))
+        ull = UnrolledLinkedList(size=2)
+        ull.append(a)
+        ull.append(b)
+        result1 = ull.getIndex()
+        ull = UnrolledLinkedList(size=2)
+        ull.append(b)
+        ull.append(a)
+        result2 = ull.getIndex()
+        self.assertEqual(result1, result2)
