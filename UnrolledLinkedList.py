012e49e4a0dca9feb42bea7eb5704a253d2a3e99
M <122533615+lj-M-github@users.noreply.github.com>
2025-02-22 22:29:26 +0300

    fix style

--- UnrolledLinkedList.py
+++ UnrolledLinkedList.py
@@ -1,11 +1,11 @@
-class Node():
-    def __init__(self, elements = None):
+class Node:
+    def __init__(self, elements=None):
         self.elements = elements if elements is not None else []
-        self.next  = None
-        self.last  = None
+        self.next = None
+        self.last = None
 
-class UnrolledLinkedList():
-    def __init__(self, size):
+class UnrolledLinkedList:
+    def __init__(self, size=4):
         self.head = None
         self.size = size
         self.current_node = None
@@ -26,79 +26,92 @@ class UnrolledLinkedList():
                 new_node.last = self.current_node
                 self.current_node = new_node
                 self.current_index = 1
-    
-    def delElement(self): #delete an element
+
+    def del_element(self): # delete an element from the last node
         if self.current_node is None or self.current_index == 0:
             return False
         
         del self.current_node.elements[self.current_index - 1]
 
-        if len(self.current_node.elements) == 1:
+        if not self.current_node.elements:
             if self.current_node.last:
+                self.current_node.last.next = self.current_node.next
+                if self.current_node.next:
+                    self.current_node.next.last = self.current_node.last
                 self.current_node = self.current_node.last
-            self.current_index = len(self.current_node.elements)
-        else:
-            self.current_index -= 1
+            else:
+                self.current_node = self.current_node.next
+
+        self.current_index = max(0, len(self.current_node.elements))
         return True
-    
-    def set(self, node_index, element_index, element): #Set a value at a specific location 
-      current = self.head
-      count = 0
 
-      while current is not None and count < node_index:
-        current = current.next
-        count += 1
-      
-      if current is None:
-         raise IndexError("Node index out of range")
-      
-      if element_index < 0 or element_index >= len(current.elements):
-         raise IndexError("Element index out of range")
-      
-      current.elements[element_index] = element
-    
-    def check(self, element): #Check the element exist or not
-      current = self.head
-      count = 0
+    def set_value(self, node_index, element_index, element): # set a value at a specific location
+        current = self.head
+        count = 0
 
-      while current is not None:
-        for e in current.elements:
-            if e == element:
-               count += 1
-        current = current.next
-      
-      return element, count 
+        while current is not None and count < node_index:
+            current = current.next
+            count += 1
 
-    
-    def to_list(self): #Convert UnrolledLinkedList into List
-      res = []
-      current = self.head
-      while current is not None:
-            res.extend(current.elements)  # Add the elements of the current node to res
-            current = current.next  # move to the next node
-      return res
-    
-    def from_list(self,list): #Convert the list into UnrolledLinkedList
-      self.head = None
-      self.current_node = None
-      self.current_index = 0
-      if not list:
-        return
-      for e in list:
-        self.append(e)
-          
-      
-    def getLastNode(self): #Get the last node return list and index？
-      if self.current_node is None:
-          return None, 0
-      else:
-          return self.current_node.elements, self.current_index # 为什么直接不返回最后一个节点或者最后一个节点的最后一个数字？
-    
-    def PrintWholeList(self): #Print whole UnrolledLinkedList
-      current = self.head
-      while current is not None:
-        print(current.elements)
-        current = current.next
-    
+        if current is None:
+            raise IndexError("Node index out of range")
 
+        if element_index < 0 or element_index >= len(current.elements):
+            raise IndexError("Element index out of range")
 
+        current.elements[element_index] = element
+
+    def get_value(self, node_index, element_index): # get value by index
+        current = self.head
+        count = 1
+
+        while current is not None and count < node_index:
+            current = current.next
+            count += 1
+
+        if current is None:
+            raise IndexError("Node index out of range")
+
+        if element_index < 0 or element_index > len(current.elements):
+            raise IndexError("Element index out of range")
+
+        return current.elements[element_index-1]
+
+    def check(self, element): # check the number of elements exist
+        current = self.head
+        count = 0
+
+        while current is not None:
+            count += current.elements.count(element)
+            current = current.next
+
+        return element, count
+
+    def to_list(self): # convert UnrolledLinkedList into List
+        res = []
+        current = self.head
+        while current is not None:
+            res.extend(current.elements)
+            current = current.next
+        return res
+
+    def from_list(self, elements_list): # convert List into UnrolledLinkedList
+        self.head = None
+        self.current_node = None
+        self.current_index = 0
+        if not elements_list:
+            return
+        for e in elements_list:
+            self.append(e)
+
+    def get_last_node(self): # get the last node return elements and index
+        if self.current_node is None:
+            return None, 0
+        else:
+            return self.current_node.elements, self.current_index
+
+    def print_whole_list(self): # print whole UnrolledLinkedList
+        current = self.head
+        while current is not None:
+            print(current.elements)
+            current = current.next
